16ebb3d290d3d46c8006024abf53f871447ba8fc
Illia Safonov <safonov.illia@gmail.com>
2025-09-29 11:50:13 +0200

    result

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split(';')
+    .map((line) => line.trim())
+    .filter((line) => line)
+    .reduce((acc, line) => {
+      const [property, value] = line.split(':');
+
+      if (property && value !== undefined) {
+        acc[property.trim()] = value.trim();
+      }
+
+      return acc;
+    }, {});
 }
 
 module.exports = convertToObject;
